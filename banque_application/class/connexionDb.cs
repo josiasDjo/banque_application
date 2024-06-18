e1ac6aa2e3573a54b95966f05c43753fe7f3a6c6
Josias Besodia <djodev23@gmail.com>
2024-06-18 17:10:47 +0200

    change2

--- banque_application/class/connexionDb.cs
+++ banque_application/class/connexionDb.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
-using System.Data;
 using System.Windows.Forms;
 
-namespace banque_application.classes
+namespace banque_application
 {
-    class Connexiondb
-                {
-                  public  static string accesbd = "server=localhost; database=COMPTABILITE_BANQUE_EXAMEN ;uid=sa ;pwd=chris";
-                }
+        class connexionDb
+    {
+        public SqlConnection reqSql;
+        public void connDb()
+        {
+            string connexion = "Data Source=DJODEV;Initial Catalog=COMPTABILITE_BANQUE_EXAMEN;Integrated Security=True";
+
+            try
+            {
+                reqSql = new SqlConnection(connexion);
+                reqSql.Open();
+            }
+            catch (SqlException exc)
+            {
+                MessageBox.Show("Une erreur s'est produite lors de la connexion : " + exc);
+            }
+        }
+    }
 }
