6225a0e3961151d70278387ec12d09e7a37c4131
Josias Besodia <djodev23@gmail.com>
2024-06-25 02:05:56 +0200

    dashboard config

--- banque_application/disign/comptabilité.Designer.cs
+++ banque_application/disign/comptabilité.Designer.cs
@@ -50,7 +50,7 @@
             this.label15 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.dettelabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -91,19 +91,18 @@
             this.txtDepenseTotal.AutoSize = true;
             this.txtDepenseTotal.BackColor = System.Drawing.Color.White;
             this.txtDepenseTotal.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDepenseTotal.ForeColor = System.Drawing.Color.Black;
+            this.txtDepenseTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(112)))), ((int)(((byte)(255)))));
             this.txtDepenseTotal.Location = new System.Drawing.Point(27, 65);
             this.txtDepenseTotal.Name = "txtDepenseTotal";
-            this.txtDepenseTotal.Size = new System.Drawing.Size(24, 19);
+            this.txtDepenseTotal.Size = new System.Drawing.Size(0, 19);
             this.txtDepenseTotal.TabIndex = 7;
-            this.txtDepenseTotal.Text = "m";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.Color.Black;
-            this.label12.Location = new System.Drawing.Point(5, 65);
+            this.label12.Location = new System.Drawing.Point(13, 65);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(18, 19);
             this.label12.TabIndex = 7;
@@ -117,9 +116,9 @@
             this.label13.ForeColor = System.Drawing.Color.Black;
             this.label13.Location = new System.Drawing.Point(3, 44);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(147, 22);
+            this.label13.Size = new System.Drawing.Size(140, 22);
             this.label13.TabIndex = 6;
-            this.label13.Text = "REVENU TOTAL";
+            this.label13.Text = "CLIENT TOTAL";
             // 
             // label10
             // 
@@ -187,7 +186,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(5, 65);
+            this.label9.Location = new System.Drawing.Point(13, 65);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(18, 19);
             this.label9.TabIndex = 7;
@@ -209,7 +208,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(5, 65);
+            this.label7.Location = new System.Drawing.Point(10, 65);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(18, 19);
             this.label7.TabIndex = 7;
@@ -244,9 +243,8 @@
             this.txtSoldeTotal.ForeColor = System.Drawing.Color.White;
             this.txtSoldeTotal.Location = new System.Drawing.Point(27, 65);
             this.txtSoldeTotal.Name = "txtSoldeTotal";
-            this.txtSoldeTotal.Size = new System.Drawing.Size(24, 19);
+            this.txtSoldeTotal.Size = new System.Drawing.Size(0, 19);
             this.txtSoldeTotal.TabIndex = 7;
-            this.txtSoldeTotal.Text = "m";
             // 
             // panel2
             // 
@@ -305,7 +303,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dettelabel);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.pictureBox1);
@@ -315,24 +313,24 @@
             this.panel1.Size = new System.Drawing.Size(192, 98);
             this.panel1.TabIndex = 11;
             // 
-            // label1
+            // dettelabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(27, 65);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(24, 19);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "m";
+            this.dettelabel.AutoSize = true;
+            this.dettelabel.BackColor = System.Drawing.Color.White;
+            this.dettelabel.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dettelabel.ForeColor = System.Drawing.Color.Black;
+            this.dettelabel.Location = new System.Drawing.Point(27, 65);
+            this.dettelabel.Name = "dettelabel";
+            this.dettelabel.Size = new System.Drawing.Size(24, 19);
+            this.dettelabel.TabIndex = 7;
+            this.dettelabel.Text = "m";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(5, 65);
+            this.label2.Location = new System.Drawing.Point(14, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(18, 19);
             this.label2.TabIndex = 7;
@@ -346,9 +344,9 @@
             this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(3, 44);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(147, 22);
+            this.label3.Size = new System.Drawing.Size(131, 22);
             this.label3.TabIndex = 6;
-            this.label3.Text = "REVENU TOTAL";
+            this.label3.Text = "DETTE TOTAL";
             // 
             // pictureBox1
             // 
@@ -420,7 +418,7 @@
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label dettelabel;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
--- banque_application/disign/comptabilité.cs
+++ banque_application/disign/comptabilité.cs
@@ -7,15 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace banque_application.disign
 {
     public partial class comptabilte : UserControl
     {
-        private string soldeTotal;
-        private string revenuTotal;
-        private string detteTotal;
-        private string clientTotal;
+        service_personnel sp = new service_personnel();
+        public decimal soldeTotal;
+        public decimal revenuTotal;
+        public decimal detteTotal;
+        public decimal clientTotal;
         public comptabilte()
         {
             InitializeComponent();
@@ -25,6 +27,62 @@ namespace banque_application.disign
         {
             DateTime currentDate = DateTime.Now;
             dateToDay.Text = currentDate.ToString("dd/MM/yyyy");
+            txtSoldeTotal.Text = "";
+            txtRevenuTotal.Text = "";
+            txtDepenseTotal.Text = "";
+            dettelabel.Text = "";
+
+            getStat();
         }
+        private void getStat()
+        {
+            try
+            {
+                sp.OpenConnection();
+                using (SqlConnection con = sp.GetConnection())
+                {
+                    string req1 = "SELECT * FROM tCompte";
+                    string req2 = "SELECT * FROM tPret";
+
+                    using (SqlCommand cmd1 = new SqlCommand(req1, con))
+                    {
+                        using (SqlDataReader rd = cmd1.ExecuteReader())
+                        {
+                            while (rd.Read())
+                            {
+                                clientTotal += decimal.Parse(rd["solde_compte"].ToString());
+                            }
+                            //MessageBox.Show("Client totale : " + clientTotal);
+                            txtDepenseTotal.Text = clientTotal.ToString();
+                        }
+                    }
+                    using (SqlCommand cmd2 = new SqlCommand(req2 ,con))
+                    {
+                        using (SqlDataReader rd = cmd2.ExecuteReader())
+                        {
+                            while (rd.Read())
+                            {
+                                detteTotal += decimal.Parse(rd["montant_restat"].ToString());
+                                revenuTotal += decimal.Parse(rd["taux_interet"].ToString());
+                            }
+                            //MessageBox.Show("Client totale : " + clientTotal);
+                            dettelabel.Text = detteTotal.ToString();
+                            txtRevenuTotal.Text = revenuTotal.ToString();
+                        }
+                    }
+                    soldeTotal = revenuTotal + detteTotal + clientTotal;
+                    txtSoldeTotal.Text = soldeTotal.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex);
+            } finally
+            {
+                sp.CloseConnection();
+            }
+        }
+
     }
 }
